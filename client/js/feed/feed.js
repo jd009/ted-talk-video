66cc20ea52fc60a39445871a8b1a40c08ca1edce
John D <deblanc.john@gmail.com>
2016-03-01 23:16:57 -0600

    Adding a modal to allow viewing the video. Also, adjusted width on the info bubble to accommodate more text.

--- client/js/feed/feed.js
+++ client/js/feed/feed.js
@@ -1,12 +1,35 @@
 angular.module('tedTalkFeedApp.feed', ['tedTalkFeedApp.feedService'])
 
-.controller('feedCtrl', function($scope, TedTalkFeedFactory){
+.controller('feedCtrl', function($scope, TedTalkFeedFactory, $sce){
   $scope.tedTalkList = null;
+  $scope.tedTalkToPlayTitle = null;
+  $scope.tedTalkToPlayUrl = null;
 
   TedTalkFeedFactory.getTedTalkList().then(function(tedTalkList) {
     console.log(tedTalkList);
     $scope.tedTalkList = tedTalkList;
   });
+
+  $scope.playTedTalk = function() {
+    $('#videoModal').modal('show');
+    $('#loading-spinner').show();
+
+    var videoPlayerDomObject = $('#videoPlayer')[0];
+    videoPlayerDomObject.addEventListener('loadeddata', function() {
+      $('#loading-spinner').hide();
+    }, false);
+
+    $scope.tedTalkToPlayTitle = this.tedTalk.title;
+    $scope.tedTalkToPlayUrl = $sce.trustAsResourceUrl(this.tedTalk.mediaURL);
+  };
+
+  $scope.closePlayingTedTalk = function() {
+    var videoPlayerDomObject = $('#videoPlayer')[0];
+    videoPlayerDomObject.pause();
+    $scope.tedTalkToPlayUrl = null;
+    $scope.tedTalkToPlayTitle = null;
+    videoPlayerDomObject.src = '';
+  };
 
   $scope.showControlIcons = function() {
     this.tedTalk.areControlIconsVisible = true;
